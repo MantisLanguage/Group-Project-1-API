3e5d842b247f11ad8933b16dd0bb62ccb8302f73
Mantis <mantis@Connors-MacBook-Pro.local>
2019-08-02 11:17:21 -0700

    Catching up

--- Connor.js
+++ Connor.js
@@ -15,32 +15,40 @@ $("#search").click(function () {
 
     }).then(function (response) {
         console.log(response);
-        for (var i = 0; i < results.length; i++) {
-            var gameIMG = $('<img>');
-            var gameRate = response.ratings;
-            var results = response.data;
-            gameIMG.attr('src', results[i].short_screenshots[1].url);
-            gameRate.attr('src', results[i].ratings.url)
-            $(".gameImage").prepend(gameIMG)
-            $(".gameRating").prepend(gameRate)
+        var results = response.results
+        console.log(results);
+         for (var i = 0; i < results.length; i++) {
+        var imgURL = results[i].short_screenshots[1].image;
+        var ratingRes = results[i].rating;
+        console.log(imgURL);
+        console.log(ratingRes);
+           $("#game-image").attr("src", imgURL);
+            $("#ratingText").text(ratingRes);
+        //    gameIMG.attr('src', results[1].short_screenshots[1].image);
+        //     gameRate.text(results[1].rating)
+            // $(".gameImage").prepend(gameIMG)
+            // $(".gameRating").prepend(gameRate)
         }});
 
-    $.ajax({
-        url: "https://cors-anywhere.herokuapp.com/https://api.twitch.tv/helix/streams?game=" + game,
-        method: "GET",
-        headers: {
-            "Client-ID":
-                "cdfnz8vncfslalcwt8h9257c540ta1"
-        },
+    // $.ajax({
+    //     url: "https://cors-anywhere.herokuapp.com/https://api.twitch.tv/helix/streams?game=" + game,
+    //     method: "GET",
+    //     headers: {
+    //         "Client-ID":
+    //             "cdfnz8vncfslalcwt8h9257c540ta1"
+
+    //             // data[""0""].thumbnail_url
+
+    //             // results[1].short_screenshots[1]
+    //     },
 
 
-    }).then(function (response) {
-        console.log(response);
-    });
+    // }).then(function (response) {
+    //     console.log(response);
+    // });
 
 });
 
 
- // Twitch Client-ID: "cdfnz8vncfslalcwt8h9257c540ta1",
 
-
+ // Twitch Client-ID: "cdfnz8vncfslalcwt8h9257c540ta1"
